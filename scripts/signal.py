55f01a5fcdee7ca8df178e46224bd00990db8413
Nikola Milosevic <nikolam495@gmail.com>
2021-05-14 17:57:53 +0200

    add signal script

---
+++ scripts/signal.py
@@ -0,0 +1,11 @@
+from mpm_sim.kspace import load_h5_signal
+
+
+def main():
+    signals_h5 = "../runs/test_patch/signals.h5"
+    _, m = load_h5_signal(signals_h5)
+    print(m)
+
+
+if __name__ == '__main__':
+    main()
